de4a8a180cde6ca6cff16066474d82150f36e891
Ecaterina Pihurov <ecaterinaoprea92@gmail.com>
2021-03-31 21:18:07 +0300

    First commit

---
+++ secret.js
@@ -0,0 +1,14 @@
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+console.log(secretMessage.length);
+secretMessage.pop();
+console.log(secretMessage);
+secretMessage.push('to', 'program');
+console.log(secretMessage);
+secretMessage[7] = 'right';
+console.log(secretMessage);
+secretMessage.shift();
+console.log(secretMessage);
+secretMessage.unshift('Programing');
+console.log(secretMessage);
+secretMessage[6, 7, 8, 9, 10] = 'know,';
+console.log(secretMessage.join());
